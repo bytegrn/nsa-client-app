571d2c680feb38cc3c8f8d45b1df42be414ee6b6
bytegrn <greenbrightsmart@gmail.com>
2025-10-04 11:09:36 +0100

    feat: login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -12,27 +12,71 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Input } from "@/components/ui/input";
+import { axiosInstance } from "@/constants";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { AlertCircleIcon } from "lucide-react";
 
 const loginFormSchema = z.object({
-  email: z.email().optional(),
-  password: z.string().optional(),
+  email: z.email(),
+  password: z.string().min(8, "Password cannot be less than 8 characters"),
 });
 
 export default function LoginForm() {
+  const [unexpectedError, setUnexpectedError] = useState(false);
+
   const form = useForm<z.infer<typeof loginFormSchema>>({
     resolver: zodResolver(loginFormSchema),
+    mode: "onChange",
     defaultValues: {
       email: undefined,
       password: undefined,
     },
   });
 
-  function onSubmit(values: z.infer<typeof loginFormSchema>) {
-    console.log(values);
+  const router = useRouter();
+
+  async function onSubmit(formValues: z.infer<typeof loginFormSchema>) {
+    try {
+      console.log(formValues);
+      const res = await axiosInstance.post("/auth/login", formValues);
+      const authenticated = res.data?.success && res.data?.authenticated;
+
+      if (authenticated) router.push("/s");
+    } catch (error) {
+      console.log(error);
+      if (axios.isAxiosError(error)) {
+        const errorCode = error.response?.data?.errorCode ?? null;
+
+        if (errorCode && Number(errorCode) === 4004) {
+          form.setError("root", {
+            message: "Email or Password incorrect",
+          });
+          return;
+        }
+      }
+      setUnexpectedError(true);
+      console.log("unexpected error:\n", error);
+    }
   }
   return (
     <Form {...form}>
+      {unexpectedError ? (
+        <Alert
+          variant={"destructive"}
+          className="absolute top-[15vh] max-w-[360px]"
+        >
+          <AlertCircleIcon />
+          <AlertTitle>Something went wrong</AlertTitle>
+          <AlertDescription>
+            An unexpected error occured, please try again later or contact
+            admin.
+          </AlertDescription>
+        </Alert>
+      ) : null}
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
         <FormField
           control={form.control}
@@ -41,7 +85,11 @@ export default function LoginForm() {
             <FormItem>
               <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input placeholder="email address" {...field} />
+                <Input
+                  placeholder="email address"
+                  {...field}
+                  onFocus={() => form.clearErrors("root")}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -54,13 +102,36 @@ export default function LoginForm() {
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input placeholder="password" {...field} />
+                <Input
+                  type="password"
+                  placeholder="password"
+                  {...field}
+                  onFocus={() => form.clearErrors("root")}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        {form.formState.isValid ? (
+          <Button type="submit" className="rounded-[8px] w-full">
+            Sign in
+          </Button>
+        ) : (
+          <Button
+            type="submit"
+            disabled
+            className="rounded-[8px] w-full"
+            variant={"inactive"}
+          >
+            Sign in
+          </Button>
+        )}
+        {form.formState.errors.root && (
+          <p className="text-ui-error-500 text-[14px] leading-5">
+            {form.formState.errors.root.message}
+          </p>
+        )}
       </form>
     </Form>
   );
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,10 @@
-export const API_BASE_URL = "https://nsaproject.onrender.com";
+import axios from "axios";
+
+export const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001/v1"
+    : "https://nsaproject.onrender.com/v1";
+
+export const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+});
