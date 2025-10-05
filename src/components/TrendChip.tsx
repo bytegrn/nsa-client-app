c1fc073ff49dee9b17f8529f300513bbcfdbe3f6
bytegrn <greenbrightsmart@gmail.com>
2025-10-05 10:19:11 +0100

    ui: update

--- src/components/TrendChip.tsx
+++ src/components/TrendChip.tsx
@@ -14,7 +14,7 @@ export default function TrendChip({ trend, percentageValue }: TrendChipProps) {
     <div className="flex gap-1 bg-ui-success-50 rounded-[16px] py-[2px] pl-[10px] pr-2 items-center">
       <ArrowUp strokeWidth={"3px"} size={"18px"} color={TrendColors[trend]} />
       <span className={`text-ui-success-700 text-[14px] font-medium leading-5`}>
-        {percentageValue}
+        {percentageValue + "%"}
       </span>
     </div>
   );
